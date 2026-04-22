eceb15954d002f20d02bda997fdd81314304fed1
kahfi rks plai <muhammadkahfiraihansaputra@gmail.com>
2026-04-22 11:22:29 +0700

    Main function

--- main.cpp
+++ main.cpp
@@ -9,3 +9,17 @@ void displayHeader() {
     cout << "Status: Active Monitoring..." << endl;
     cout << "--------------------------------" << endl;
 }
+
+int main() {
+    displayHeader();
+    string password = "Abcde!2345";
+    string cipherText = encryptData(password);
+    
+    cout << "Encrypted password is " << cipherText << endl;
+    
+    if (isSecure(password)) {
+        cout << "Password is secure!" << endl;
+    } else {
+        cout << "Password is not secure!" << endl;
+    }
+}
